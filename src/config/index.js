9d88d1d3d3ab81e86239870888af0d2a8e1dd66a
Max Weng <max.m.weng@gmail.com>
2018-06-16 18:45:00 +0800

    Updated to support https

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const chainOptions = {
     debug: true,
-    gethRpc: "http://192.168.1.100:8545",
-    fastXRpc: "http://dev2.msan.cn:8546/jsonrpc",
+    gethRpc: "//localhost:8545",
+    fastXRpc: "//fastx-rinkeby.msan.cn/jsonrpc",
     rootChainAddress: "0xD9FA1cbB70b74f3Ef259CE0eb48029F02eE0FcD1",
 }
