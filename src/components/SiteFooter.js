7906e958d900df88e19aaa62defba69c4367faeb
Max Weng <max.m.weng@gmail.com>
2018-06-16 18:42:19 +0800

    Removed footer placeholders

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -22,50 +22,15 @@ export default class SiteFooter extends Component {
             <Segment
                 inverted
                 vertical
-                style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
+                style={{ margin: '5em 0em 0em', padding: '0em 0em 5em' }}
             >
                 <Container textAlign='center'>
-                    <Grid divided inverted stackable>
-                        <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Group 1' />
-                            <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
-                            <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Group 2' />
-                            <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
-                            <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Group 3' />
-                            <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
-                            <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Footer Header' />
-                            <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                        </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-        
                     <Divider inverted section />
                     <Image
                         centered
                         size='mini'
                         src='/logo.png'
+                        style={{padding: '0 0 2em'}}
                     />
                     <List horizontal inverted divided link>
                         <List.Item as='a' href='#'>Site Map</List.Item>
