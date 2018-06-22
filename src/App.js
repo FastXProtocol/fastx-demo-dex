8bcf63e4a918adf5843ec35ad2784a63c704b800
twygod <twygod@gmail.com>
2018-06-22 11:38:47 +0800

    集成react-web3

--- src/App.js
+++ src/App.js
@@ -8,20 +8,34 @@ import AssetList from './containers/AssetList';
 import AssetDetail from './containers/AssetDetail';
 import AssetSell from './containers/AssetSell';
 import Account from './containers/Account';
+import Web3 from 'web3';
+import { Web3Provider } from 'react-web3';
+import { chainOptions } from './config';
+
+// If the browser has injected Web3.JS
+if (window.web3 && window.web3.currentProvider) {
+  // Then replace the old injected version by the local Web3.JS version 1.0.0-beta.N
+  setTimeout(function(){
+    window.web3 = new Web3(window.web3.currentProvider);
+    console.log(window.web3.version)
+  }, 0) 
+}
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SiteHeader />
-        <Switch>
-          <Route path='/account' component={Account} />
-          <Route exact path='/assets' component={AssetList} />
-          <Route exact path='/assets/:category/:id' component={AssetDetail} />
-          <Route exact path='/assets/:category/:id/sell' component={AssetSell} />
-          {/* <Route path='/b/:hash/:name/' component={withRouter(WithBoard(BoardPage))} /> */}
-          <Redirect path="/" to={{pathname: '/assets'}} />
-        </Switch>
+          <Web3Provider>
+            <Switch>
+              <Route path='/account' component={Account} />
+              <Route exact path='/assets' component={AssetList} />
+              <Route exact path='/assets/:category/:id' component={AssetDetail} />
+              <Route exact path='/assets/:category/:id/sell' component={AssetSell} />
+              {/* <Route path='/b/:hash/:name/' component={withRouter(WithBoard(BoardPage))} /> */}
+              <Redirect path="/" to={{pathname: '/assets'}} />
+            </Switch>
+          </Web3Provider>
         <SiteFooter />
       </div>
     );
