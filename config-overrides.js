566e690b29fefc51767f24885f9bcc88d0acc919
Max Weng <max.m.weng@gmail.com>
2018-07-08 00:10:40 +0800

    Set 'env' to 'local' in package.json to develop locally.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,9 @@
-module.exports = function override(config, env) {
+const rewireDefinePlugin = require('react-app-rewire-define-plugin')
 
+module.exports = function override(config, env) {
+  
+  config = rewireDefinePlugin(config, env, {
+    'process.env.ENV': JSON.stringify(require('./package.json').env || 'local')
+  })
   return config;
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,21 @@
-export const chainOptions = {
-    debug: true,
-    gethRpc: "//localhost:8545",
-    fastXRpc: "//localhost:8546/jsonrpc",
-    rootChainAddress: "0xa3b2a1804203b75b494028966c0f62e677447a39",
+export let chainOptions = {
+    debug: true
+}
+console.log(process.env.ENV);
+if (process.env.ENV.toLowerCase() == "local") {
+    chainOptions = {
+        ...chainOptions,
+        gethRpc: "//localhost:8545",
+        fastXRpc: "//localhost:8546/jsonrpc",
+        rootChainAddress: "0xa3b2a1804203b75b494028966c0f62e677447a39",
+    }
+} else {
+    chainOptions = {
+        ...chainOptions,
+        gethRpc: "//localhost:8545",
+        fastXRpc: "//fastx-rinkeby.msan.cn/jsonrpc",
+        rootChainAddress: "0xC47e711ac6A3D16Db0826c404d8C5d8bDC01d7b1",
+    }
 }
 
 export const chainCategory = "0xd641205E8F36A858c5867945782C917E3F63d1e8"
