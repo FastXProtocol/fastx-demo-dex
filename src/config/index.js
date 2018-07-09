f90bbd523fa49c36345ad4695565ddc212a151d9
twygod <twygod@gmail.com>
2018-07-09 12:01:09 +0800

    配置文件修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 export let chainOptions = {
     debug: true
 }
+
+export let chainCategory = ""
+
 console.log(process.env.ENV);
 if (process.env.ENV.toLowerCase() == "local") {
     chainOptions = {
@@ -9,6 +12,8 @@ if (process.env.ENV.toLowerCase() == "local") {
         fastXRpc: "//localhost:8546/jsonrpc",
         rootChainAddress: "0xa3b2a1804203b75b494028966c0f62e677447a39",
     }
+
+    chainCategory = "0xd641205E8F36A858c5867945782C917E3F63d1e8"
 } else {
     chainOptions = {
         ...chainOptions,
@@ -16,9 +21,9 @@ if (process.env.ENV.toLowerCase() == "local") {
         fastXRpc: "//fastx-rinkeby.msan.cn/jsonrpc",
         rootChainAddress: "0xC47e711ac6A3D16Db0826c404d8C5d8bDC01d7b1",
     }
-}
 
-export const chainCategory = "0xd641205E8F36A858c5867945782C917E3F63d1e8"
+    chainCategory = "0x952CE607bD9ab82e920510b2375cbaD234d28c8F"
+}
 
 export const retry = {
 	count: 5,
